4d8bc9f38c8e30164eed20741919ad16cf577bc3
Anderson-Reges <andersonreges76@gmail.com>
2022-09-19 17:20:38 -0300

    Feature: req4 complete

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém o texto Page requested not found;', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      name: /page requested not found/i, level: 2,
+    });
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
