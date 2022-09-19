5c3ccedc22f2fc34438eb11921f3fd4c5024585a
Anderson-Reges <andersonreges76@gmail.com>
2022-09-19 16:56:27 -0300

    Feature: req2 fix

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+// import { screen } from '@testing-library/react';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('teste se aparece na tela a frase No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+});
