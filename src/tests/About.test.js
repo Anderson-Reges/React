d65a1a67b83480b329fc6fd885a513e02786ffcf
Anderson-Reges <andersonreges76@gmail.com>
2022-09-19 16:35:22 -0300

    Feature: req2 complete

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex;', () => {
+    renderWithRouter(<About />);
+    const headingAbout = screen.getByRole('heading', {
+      name: /about pokédex/i, level: 2,
+    });
+    const image = screen.getByRole('img');
+
+    expect(headingAbout).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toBeInTheDocument();
+  });
+});
