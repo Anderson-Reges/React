5be44c6339932f40cff4a810b74b6b99db92cc01
Anderson-Reges <andersonreges76@gmail.com>
2022-09-19 17:08:32 -0300

    Feature: req3 complete

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,11 @@
+import { screen } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
-// import { screen } from '@testing-library/react';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   test('teste se aparece na tela a frase No favorite pokemon found', () => {
     renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
   });
 });
